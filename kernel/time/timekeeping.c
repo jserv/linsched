1abea51529b15619175f1a8d97a6faa21fbdf1fb
Paul Turner <pjt@google.com>
2012-03-14 04:09:14 -0700

    time: correct rounding in update_wall_time()
    The update to xtime.tv_nsec in update_wall_time() rounds up to the nearest
    nanosecond.  However, this code incorrectly rounds even when there is no
    remainder resulting in a very slight dilation in wall-time.  This occurs
    specifically in the case where timekeeper is ns accurate, e.g.
    timekeeper.shift == 0
    
    Signed-off-by: Paul Turner <pjt@google.com>

--- kernel/time/timekeeping.c
+++ kernel/time/timekeeping.c
@@ -1040,7 +1040,8 @@ static void update_wall_time(void)
 	 * Store full nanoseconds into xtime after rounding it up and
 	 * add the remainder to the error difference.
 	 */
-	xtime.tv_nsec =	((s64) timekeeper.xtime_nsec >> timekeeper.shift) + 1;
+	xtime.tv_nsec = ((s64)timekeeper.xtime_nsec +
+			 (1ULL << timekeeper.shift) - 1) >> timekeeper.shift;
 	timekeeper.xtime_nsec -= (s64) xtime.tv_nsec << timekeeper.shift;
 	timekeeper.ntp_error +=	timekeeper.xtime_nsec <<
 				timekeeper.ntp_error_shift;
